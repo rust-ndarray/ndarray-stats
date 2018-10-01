3bd8dc10915826d070909631748d4b1137123911
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-10-01 13:17:12 +0100

    Bins have to be generic with respect to element type T

--- src/histogram.rs
+++ src/histogram.rs
@@ -2,16 +2,16 @@ mod bins {
     use ndarray::prelude::*;
     use ndarray::Data;
 
-    struct Bins1d {
-        bins: Vec<Bin1d>,
+    struct Bins1d<T> {
+        bins: Vec<Bin1d<T>>,
     }
-    struct BinsNd {
-        bins: Vec<BinNd>,
+    struct BinsNd<T> {
+        bins: Vec<BinNd<T>>,
     }
 
     /// `Bins` is a collection of non-overlapping
     /// sub-regions (`Bin`) in a `n` dimensional space.
-    pub trait Bins {
+    pub trait Bins<T> {
         /// Return `n`, the number of dimensions.
         fn ndim(&self) -> usize;
 
@@ -20,21 +20,21 @@ mod bins {
         /// - `None`, if `P` does not belong to any `Bin` in `Bins`.
         /// 
         /// **Panics** if `P.ndim()` is different from `Bins.ndim()`. 
-        fn find<A, S, D, B>(&self, point: ArrayBase<S, D>) -> Option<B>
+        fn find<S, D, B>(&self, point: ArrayBase<S, D>) -> Option<B>
         where
-            S: Data<Elem = A>,
+            S: Data<Elem = T>,
             D: Dimension,
             B: Bin;
     }
 
-    impl Bins for Bins1d {
+    impl<T> Bins<T> for Bins1d<T> {
         fn ndim(&self) -> usize {
             1
         }
 
-        fn find<A, S, D, B>(&self, _point: ArrayBase<S, D>) -> Option<B>
+        fn find<S, D, B>(&self, _point: ArrayBase<S, D>) -> Option<B>
         where
-            S: Data<Elem = A>,
+            S: Data<Elem = T>,
             D: Dimension,
             B: Bin,
         {
@@ -42,14 +42,14 @@ mod bins {
         }
     }
 
-    impl Bins for BinsNd {
+    impl<T> Bins<T> for BinsNd<T> {
         fn ndim(&self) -> usize {
             unimplemented!() 
         }
 
-        fn find<A, S, D, B>(&self, _point: ArrayBase<S, D>) -> Option<B>
+        fn find<S, D, B>(&self, _point: ArrayBase<S, D>) -> Option<B>
         where
-            S: Data<Elem = A>,
+            S: Data<Elem = T>,
             D: Dimension,
             B: Bin,
         {
@@ -60,10 +60,15 @@ mod bins {
     pub trait Bin {
 
     }
-    struct Bin1d {}
-    struct BinNd {}
-    impl Bin for Bin1d {}
-    impl Bin for BinNd {}
+    struct Bin1d<T> {
+        left: T,
+        right: T,
+    }
+    struct BinNd<T> {
+        projections: Vec<Bin1d<T>>,
+    }
+    impl<T> Bin for Bin1d<T> {}
+    impl<T> Bin for BinNd<T> {}
 }
 
 pub trait HistogramExt {
