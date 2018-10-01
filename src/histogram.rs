d2b29d10a32faf3e2f52fc00d0587ad0513024d0
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-10-01 14:41:58 +0100

    Getting rid of Bin and Bins traits, for now

--- src/histogram.rs
+++ src/histogram.rs
@@ -2,80 +2,67 @@ mod bins {
     use ndarray::prelude::*;
     use ndarray::Data;
 
-    struct Bins1d<T> {
+    /// `Bins` is a collection of non-overlapping 
+    /// intervals (`Bin1d`) in a 1-dimensional space.
+    pub struct Bins1d<T> {
         bins: Vec<Bin1d<T>>,
     }
-    struct BinsNd<T> {
+    /// `Bins` is a collection of non-overlapping 
+    /// sub-regions (`BinNd`) in a `n`-dimensional space.
+    pub struct BinsNd<T> {
         bins: Vec<BinNd<T>>,
     }
 
-    /// `Bins` is a collection of non-overlapping /// sub-regions (`Bin`) in a `n` dimensional space.
-    pub trait Bins<T> {
+    impl<T> Bins1d<T> {
+        /// Given a point `P`, it returns an `Option`:
+        /// - `Some(B)`, if `P` belongs to the `Bin` `B`;
+        /// - `None`, if `P` does not belong to any `Bin` in `Bins`.
+        /// 
+        /// **Panics** if `P.ndim()` is different from `Bins.ndim()`. 
+        pub fn find<S, D>(&self, _point: ArrayBase<S, D>) -> Option<Bin1d<T>>
+        where
+            S: Data<Elem = T>,
+            D: Dimension,
+        {
+            unimplemented!()
+        }
+    }
+
+    impl<T> BinsNd<T> {
         /// Return `n`, the number of dimensions.
-        fn ndim(&self) -> usize;
+        fn ndim(&self) -> usize {
+            unimplemented!() 
+        }
 
         /// Given a point `P`, it returns an `Option`:
         /// - `Some(B)`, if `P` belongs to the `Bin` `B`;
         /// - `None`, if `P` does not belong to any `Bin` in `Bins`.
         /// 
         /// **Panics** if `P.ndim()` is different from `Bins.ndim()`. 
-        fn find<S, D, B>(&self, point: ArrayBase<S, D>) -> Option<B>
+        pub fn find<S, D>(&self, _point: ArrayBase<S, D>) -> Option<BinNd<T>>
         where
             S: Data<Elem = T>,
             D: Dimension,
-            B: Bin;
-    }
-
-    impl<T> Bins<T> for Bins1d<T> {
-        fn ndim(&self) -> usize {
-            1
-        }
-
-        fn find<S, D, B>(&self, _point: ArrayBase<S, D>) -> Option<B>
-        where
-            S: Data<Elem = T>,
-            D: Dimension,
-            B: Bin,
         {
             unimplemented!()
         }
     }
 
-    impl<T> Bins<T> for BinsNd<T> {
-        fn ndim(&self) -> usize {
-            unimplemented!() 
-        }
-
-        fn find<S, D, B>(&self, _point: ArrayBase<S, D>) -> Option<B>
-        where
-            S: Data<Elem = T>,
-            D: Dimension,
-            B: Bin,
-        {
-            unimplemented!()
-        }
-    }
-
-    pub trait Bin {
-
-    }
-    struct Bin1d<T> {
+    pub struct Bin1d<T> {
         left: T,
         right: T,
     }
-    struct BinNd<T> {
+    pub struct BinNd<T> {
         projections: Vec<Bin1d<T>>,
     }
-    impl<T> Bin for Bin1d<T> {}
-    impl<T> Bin for BinNd<T> {}
 }
 
 use std::collections::HashMap;
-use self::bins::{Bin, Bins};
+use self::bins::{Bin1d, BinNd, BinsNd, Bins1d};
 use ndarray::prelude::*;
 use ndarray::Data;
 
-type Histogram = HashMap<Box<Bin>, usize>;
+type HistogramNd<T> = HashMap<BinNd<T>, usize>;
 
 pub trait HistogramNdExt<A, S>
 where
@@ -91,18 +78,14 @@ where
     /// 
     /// For example: a (3, 4) matrix `M` is a collection of 3 points in a 
     /// 4-dimensional space.
-    fn histogram<B>(&self, bins: B) -> Histogram
-    where
-        B: Bins<A>;
+    fn histogram<B>(&self, bins: BinsNd<A>) -> HistogramNd<A>;
 }
 
 impl<A, S> HistogramNdExt<A, S> for ArrayBase<S, Ix2>
 where
     S: Data<Elem = A>,
 {
-    fn histogram<B>(&self, bins: B) -> Histogram
-    where
-        B: Bins<A>,
+    fn histogram<B>(&self, bins: BinsNd<A>) -> HistogramNd<A>
     {
         let mut histogram = HashMap::new();
         for point in self.axis_iter(Axis(0)) {
@@ -115,22 +98,20 @@ where
     }
 }
 
+type Histogram1d<T> = HashMap<Bin1d<T>, usize>;
+
 pub trait Histogram1dExt<A, S>
 where
     S: Data<Elem = A>,
 {
-    fn histogram<B>(&self, bins: B) -> Histogram
-    where
-        B: Bins<A>;
+    fn histogram<B>(&self, bins: Bins1d<A>) -> Histogram1d<A>;
 }
 
 impl<A, S> Histogram1dExt<A, S> for ArrayBase<S, Ix1>
 where
     S: Data<Elem = A>,
 {
-    fn histogram<B>(&self, _bins: B) -> Histogram
-    where
-        B: Bins<A>,
+    fn histogram<B>(&self, bins: Bins1d<A>) -> Histogram1d<A>
     {
         unimplemented!()
     }
