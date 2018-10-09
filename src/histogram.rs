3cdb3badec251709e7ee7e71652e10ea428155f9
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-10-09 08:53:44 +0100

    Adding missing pieces - now it compiles

--- src/histogram.rs
+++ src/histogram.rs
@@ -1,24 +1,74 @@
-/// Wrapper around `Array1` that makes sure the elements are in ascending order.
-struct Edges<A: Ord> {
-    edges: Array1<A>,
+use ndarray::prelude::*;
+use std::ops::Range;
+use std::error;
+use std::fmt;
+
+#[derive(Debug, Clone)]
+struct BinNotFound;
+
+impl fmt::Display for BinNotFound {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "No bin has been found.")
+    }
 }
 
-impl<A: Ord> From<Array1<A>> for Edges<A> {
-    fn from(mut edges: Array1<A>) -> Self {
+impl error::Error for BinNotFound {
+    fn description(&self) -> &str {
+        "No bin has been found."
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
+}
+
+/// Wrapper around `Vec` that makes sure the elements are in ascending order.
+struct Edges<A: Ord> {
+    edges: Vec<A>,
+}
+
+impl<A: Ord + Clone> From<Array1<A>> for Edges<A> {
+    fn from(edges: Array1<A>) -> Self {
+        let mut edges = edges.to_vec();
         // sort the array in-place
+        edges.sort_unstable();
+        Edges { edges }
+    }
+}
+
+impl<A: Ord> From<Vec<A>> for Edges<A> {
+    fn from(mut edges: Vec<A>) -> Self {
+        // sort the array in-place
+        edges.sort_unstable();
         Edges { edges }
     }
 }
 
 impl<A: Ord> Edges<A> {
-    fn view(&self) -> ArrayView1<A> {
-        self.edges.view()
+    fn len(&self) -> usize {
+        self.edges.len()
+    }
+
+    fn slice(&self) -> &[A] {
+        &self.edges
     }
 
     /// Returns the index of the bin containing the given value,
     /// or `None` if none of the bins contain the value.
     fn bin_index(&self, value: &A) -> Option<usize> {
         // binary search for the correct bin
+        let n = self.len();
+        match self.edges.binary_search(value) {
+            Ok(i) => Some(i),
+            Err(i) => {
+                match i {
+                    0 => None,
+                    j if j == n => None,
+                    _ => Some(i - 1),
+                }
+            }
+        }
     }
 
     /// Returns the range of the bin containing the given value.
@@ -27,7 +77,13 @@ impl<A: Ord> Edges<A> {
         A: Clone,
     {
         let i = self.bin_index(value);
-        Range { start: self.edges[i].clone(), end: self.edges[i + 1].clone() }
+        match i {
+            Some(j) => Some(
+                Range { start: self.edges[j].clone(),
+                        end: self.edges[j + 1].clone() }
+            ),
+            None => None,
+        }
     }
 }
 
@@ -47,12 +103,13 @@ impl<A: Ord> HistogramCounts<A> {
         HistogramCounts { counts, edges }
     }
 
-    pub fn add_observation(observation: ArrayView1<A>) -> Result<(), NotFound> {
+    pub fn add_observation(&mut self, observation: ArrayView1<A>) -> Result<(), BinNotFound> {
         let bin = observation
             .iter()
             .zip(&self.edges)
-            .map(|(v, e)| e.bin_index(v).ok_or(NotFound))
+            .map(|(v, e)| e.bin_index(v).ok_or(BinNotFound))
             .collect::<Result<Vec<_>, _>>()?;
-        self.counts[bin] += 1;
+        self.counts[IxDyn(&bin)] += 1;
+        Ok(())
     }
 }
