bbaee7df25d43018bb8c9a2cda91b8c2a0b950c6
LukeMathWalker <lpalmieri@truelayer.com>
2018-10-13 11:54:39 +0100

    Fixed bugs, better method names, exported methods needed for doc tests.

--- src/histogram.rs
+++ src/histogram.rs
@@ -4,7 +4,7 @@ use std::error;
 use std::fmt;
 
 #[derive(Debug, Clone)]
-struct BinNotFound;
+pub struct BinNotFound;
 
 impl fmt::Display for BinNotFound {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -23,8 +23,31 @@ impl error::Error for BinNotFound {
     }
 }
 
-/// Wrapper around `Vec` that makes sure the elements are in ascending order.
-struct Edges<A: Ord> {
+/// `Edges` is a sorted collection of `A` elements used
+/// to represent the boundaries of intervals on
+/// a 1-dimensional axis.
+///
+/// # Example:
+///
+/// ```
+/// extern crate ndarray_stats;
+/// extern crate noisy_float;
+/// use ndarray_stats::Edges;
+/// use noisy_float::types::n64;
+///
+/// let unit_interval = Edges::from(vec![n64(0.), n64(1.)]);
+/// // left inclusive
+/// assert_eq!(
+///     unit_interval.bin_range(&n64(0.)).unwrap(),
+///     n64(0.)..n64(1.),
+/// );
+/// // right exclusive
+/// assert_eq!(
+///     unit_interval.bin_range(&n64(1.)),
+///     None
+/// );
+/// ```
+pub struct Edges<A: Ord> {
     edges: Vec<A>,
 }
 
@@ -44,48 +67,64 @@ impl<A: Ord + Clone> From<Array1<A>> for Edges<A> {
 }
 
 impl<A: Ord> Edges<A> {
-    fn len(&self) -> usize {
+    pub fn n_intervals(&self) -> usize {
+        match self.n_edges() {
+            0 => 0,
+            n => n - 1,
+        }
+    }
+
+    pub fn n_edges(&self) -> usize {
         self.edges.len()
     }
 
-    fn slice(&self) -> &[A] {
+    pub fn slice(&self) -> &[A] {
         &self.edges
     }
 
     /// Returns the index of the bin containing the given value,
     /// or `None` if none of the bins contain the value.
-    fn bin_index(&self, value: &A) -> Option<usize> {
+    fn edges_indexes(&self, value: &A) -> Option<(usize, usize)> {
         // binary search for the correct bin
-        let n = self.len();
+        let n_edges = self.n_edges();
         match self.edges.binary_search(value) {
-            Ok(i) => Some(i),
+            Ok(i) if i == n_edges-1 => None,
+            Ok(i) => Some((i, i+1)),
             Err(i) => {
                 match i {
                     0 => None,
-                    j if j == n => None,
-                    _ => Some(i - 1),
+                    j if j == n_edges => None,
+                    j => Some((j-1, j)),
                 }
             }
         }
     }
 
+    /// Returns the index of the bin containing the given value,
+    /// or `None` if none of the bins contain the value.
+    fn bin_index(&self, value: &A) -> Option<usize> {
+        self.edges_indexes(value).map(|t| t.0)
+    }
+
     /// Returns the range of the bin containing the given value.
-    fn bin_range(&self, value: &A) -> Option<Range<A>>
+    pub fn bin_range(&self, value: &A) -> Option<Range<A>>
     where
         A: Clone,
     {
-        let i = self.bin_index(value);
-        match i {
-            Some(j) => Some(
-                Range { start: self.edges[j].clone(),
-                        end: self.edges[j + 1].clone() }
-            ),
-            None => None,
-        }
+        let edges_indexes= self.edges_indexes(value);
+        edges_indexes.map(
+            |t| {
+                let (left, right) = t;
+                Range {
+                    start: self.edges[left].clone(),
+                    end: self.edges[right].clone()
+                }
+            }
+        )
     }
 }
 
-struct HistogramCounts<A: Ord> {
+pub struct HistogramCounts<A: Ord> {
     counts: ArrayD<usize>,
     edges: Vec<Edges<A>>,
 }
@@ -97,7 +136,7 @@ struct HistogramDensity<A: Ord> {
 
 impl<A: Ord> HistogramCounts<A> {
     pub fn new(edges: Vec<Edges<A>>) -> Self {
-        let counts = ArrayD::zeros(edges.iter().map(|e| e.len() - 1).collect::<Vec<_>>());
+        let counts = ArrayD::zeros(edges.iter().map(|e| e.n_intervals()).collect::<Vec<_>>());
         HistogramCounts { counts, edges }
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub use maybe_nan::{MaybeNan, MaybeNanExt};
 pub use quantile::{interpolate, QuantileExt};
 pub use sort::Sort1dExt;
 pub use correlation::CorrelationExt;
+pub use histogram::{Edges, HistogramCounts, BinNotFound};
 
 mod maybe_nan;
 mod quantile;
