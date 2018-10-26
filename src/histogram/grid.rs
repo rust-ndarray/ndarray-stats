4d5119c102a9b6f4a0935fbf76c748dedf9c17b1
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-10-26 08:32:02 +0100

    grid.iter renamed to iter_projections

--- src/histogram/grid.rs
+++ src/histogram/grid.rs
@@ -1,5 +1,7 @@
 use super::bins::Bins;
+use super::errors::BinNotFound;
 use std::slice::Iter;
+use ndarray::ArrayView1;
 
 pub struct Grid<A: Ord> {
     projections: Vec<Bins<A>>,
@@ -24,5 +26,16 @@ impl<A: Ord> Grid<A> {
 
     pub fn projections(&self) -> &[Bins<A>] {
         &self.projections
+    }
+
+    pub fn index(&self, observation: ArrayView1<A>) -> Result<Vec<usize>, BinNotFound> {
+        assert_eq!(observation.len(), self.ndim(),
+                   "Dimension mismatch: the observation has {0:?} dimensions, the grid \
+                   instead has {1:?} dimensions.", observation.len(), self.ndim());
+        observation
+            .iter()
+            .zip(self.grid.iter_projections())
+            .map(|(v, e)| e.index(v).ok_or(BinNotFound))
+            .collect::<Result<Vec<_>, _>>()?
     }
 }
