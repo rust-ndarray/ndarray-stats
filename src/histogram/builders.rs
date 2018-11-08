a48afbb52a2d122efb182cfed1ab532b8545c418
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-11-08 08:42:30 +0000

    Added detailed docs to all strategies using NumPy as base (almost verbatim)

--- src/histogram/builders.rs
+++ src/histogram/builders.rs
@@ -57,24 +57,53 @@ struct EquiSpaced<T> {
 
 /// Square root (of data size) strategy, used by Excel and other programs
 /// for its speed and simplicity.
+///
+/// Let `n` be the number of observations. Then
+///
+/// `n_bins` = `sqrt(n)`
 pub struct Sqrt<T> {
     builder: EquiSpaced<T>,
 }
 
 /// A strategy that does not take variability into account, only data size. Commonly
 /// overestimates number of bins required.
+///
+/// Let `n` be the number of observations and `n_bins` the number of bins.
+///
+/// `n_bins` = 2 * `n^(1/3)`
+///
+/// `n_bins` is only proportional to cube root of `n`. It tends to overestimate
+/// the `n_bins` and it does not take into account data variability.
 pub struct Rice<T> {
     builder: EquiSpaced<T>,
 }
 
 /// R’s default strategy, only accounts for data size. Only optimal for gaussian data and
 /// underestimates number of bins for large non-gaussian datasets.
+///
+/// Let `n` be the number of observations.
+/// The number of bins is the base 2 log of `n`. This estimator assumes normality of data and
+/// is too conservative for larger, non-normal datasets.
+///
+/// This is the default method in R’s hist method.
 pub struct Sturges<T> {
     builder: EquiSpaced<T>,
 }
 
 /// Robust (resilient to outliers) strategy that takes into
 /// account data variability and data size.
+///
+/// Let `n` be the number of observations.
+///
+/// `bin_width` = 2 * `IQR` / `n^(1/3)`
+///
+/// The bin width is proportional to the interquartile range ([`IQR`]) and inversely proportional to
+/// cube root of `n`. It can be too conservative for small datasets, but it is quite good for
+/// large datasets.
+///
+/// The [`IQR`] is very robust to outliers.
+///
+/// [`IQR`]: https://en.wikipedia.org/wiki/Interquartile_range
 pub struct FreedmanDiaconis<T> {
     builder: EquiSpaced<T>,
 }
@@ -86,6 +115,11 @@ enum SturgesOrFD<T> {
 
 /// Maximum of the [`Sturges`] and [`FreedmanDiaconis`] strategies.
 /// Provides good all around performance.
+///
+/// A compromise to get a good value. For small datasets the [`Sturges`] value will usually be chosen,
+/// while larger datasets will usually default to [`FreedmanDiaconis`]. Avoids the overly
+/// conservative behaviour of [`FreedmanDiaconis`] and [`Sturges`] for
+/// small and large datasets respectively.
 ///
 /// [`Sturges`]: struct.Sturges.html
 /// [`FreedmanDiaconis`]: struct.FreedmanDiaconis.html
