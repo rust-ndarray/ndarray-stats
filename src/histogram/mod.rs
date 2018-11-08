543742da63a848f7095de59d78d8a246fce151c9
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-11-08 08:45:59 +0000

    Errors is not top-level in histogram

--- src/histogram/mod.rs
+++ src/histogram/mod.rs
@@ -2,10 +2,9 @@
 pub use self::histograms::{Histogram, HistogramExt};
 pub use self::bins::{Edges, Bins};
 pub use self::grid::{Grid, GridBuilder};
-pub use self::errors::BinNotFound;
 
 mod histograms;
 mod bins;
 pub mod strategies;
 mod grid;
-mod errors;
+pub mod errors;
