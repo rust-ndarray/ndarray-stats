cc2376dd9a38b1a0094a27064f137976416d2ca4
Jim Turner <github@turner.link>
2018-11-11 22:35:17 +0000

    Return false for failing tests in quickcheck
    Co-Authored-By: LukeMathWalker <LukeMathWalker@users.noreply.github.com>

--- src/histogram/bins.rs
+++ src/histogram/bins.rs
@@ -381,7 +381,9 @@ mod edges_tests {
             let edges = Edges::from(a);
             let n = edges.len();
             for i in 1..n {
-                assert!(edges[i-1] <= edges[i]);
+                if edges[i-1] > edges[i]) {
+                    return false;
+                }
             }
             true
         }
