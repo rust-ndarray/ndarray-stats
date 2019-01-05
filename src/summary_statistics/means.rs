8a38fbe842b2c6d788d323507fb75d8e2cb7d3be
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-01-05 16:44:59 +0000

    Added documentation

--- src/summary_statistics/means.rs
+++ src/summary_statistics/means.rs
@@ -11,10 +11,32 @@ pub trait SummaryStatisticsExt<A, S, D>
         S: Data<Elem = A>,
         D: Dimension,
 {
+    /// Returns the [`arithmetic mean`] x̅ of all elements in the array:
+    ///
+    /// ```text
+    ///     1   n
+    /// x̅ = ―   ∑ xᵢ
+    ///     n  i=1
+    /// ```
+    ///
+    /// If the array is empty, an `Err` is returned.
+    ///
+    /// [`arithmetic mean`]: https://en.wikipedia.org/wiki/Arithmetic_mean
     fn mean(&self) -> Result<A, &'static str>
     where
         A: Clone + FromPrimitive + Add<Output=A> + Div<Output=A> + PartialEq + Zero;
 
+    /// Returns the [`harmonic mean`] `HM(X)` of all elements in the array:
+    ///
+    /// ```text
+    ///             n
+    /// HM(X) = n ( ∑ xᵢ⁻¹)⁻¹
+    ///            i=1
+    /// ```
+    ///
+    /// If the array is empty, an `Err` is returned.
+    ///
+    /// [`harmonic mean`]: https://en.wikipedia.org/wiki/Harmonic_mean
     fn harmonic_mean(&self) -> Result<A, &'static str>
     where
         A: Float + FromPrimitive + PartialEq;
