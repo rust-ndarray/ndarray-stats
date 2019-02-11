ab8d701311440488142c7d031d2de926032c6023
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-02-11 08:48:34 +0000

    Fixed tests

--- tests/quantile.rs
+++ tests/quantile.rs
@@ -115,49 +115,48 @@ fn test_max_skipnan_all_nan() {
 #[test]
 fn test_quantile_axis_mut_with_odd_axis_length() {
     let mut a = arr2(&[[1, 3, 2, 10], [2, 4, 3, 11], [3, 5, 6, 12]]);
-    let p = a.quantile_axis_mut::<Lower>(Axis(0), n64(0.5));
+    let p = a.quantile_axis_mut::<Lower>(Axis(0), n64(0.5)).unwrap();
     assert!(p == a.index_axis(Axis(0), 1));
 }
 
 #[test]
-#[should_panic]
 fn test_quantile_axis_mut_with_zero_axis_length() {
     let mut a = Array2::<i32>::zeros((5, 0));
-    a.quantile_axis_mut::<Lower>(Axis(1), n64(0.5));
+    assert!(a.quantile_axis_mut::<Lower>(Axis(1), n64(0.5)).is_none());
 }
 
 #[test]
 fn test_quantile_axis_mut_with_empty_array() {
     let mut a = Array2::<i32>::zeros((5, 0));
-    let p = a.quantile_axis_mut::<Lower>(Axis(0), n64(0.5));
+    let p = a.quantile_axis_mut::<Lower>(Axis(0), n64(0.5)).unwrap();
     assert_eq!(p.shape(), &[0]);
 }
 
 #[test]
 fn test_quantile_axis_mut_with_even_axis_length() {
     let mut b = arr2(&[[1, 3, 2, 10], [2, 4, 3, 11], [3, 5, 6, 12], [4, 6, 7, 13]]);
-    let q = b.quantile_axis_mut::<Lower>(Axis(0), n64(0.5));
+    let q = b.quantile_axis_mut::<Lower>(Axis(0), n64(0.5)).unwrap();
     assert!(q == b.index_axis(Axis(0), 1));
 }
 
 #[test]
 fn test_quantile_axis_mut_to_get_minimum() {
     let mut b = arr2(&[[1, 3, 22, 10]]);
-    let q = b.quantile_axis_mut::<Lower>(Axis(1), n64(0.));
+    let q = b.quantile_axis_mut::<Lower>(Axis(1), n64(0.)).unwrap();
     assert!(q == arr1(&[1]));
 }
 
 #[test]
 fn test_quantile_axis_mut_to_get_maximum() {
     let mut b = arr1(&[1, 3, 22, 10]);
-    let q = b.quantile_axis_mut::<Lower>(Axis(0), n64(1.));
+    let q = b.quantile_axis_mut::<Lower>(Axis(0), n64(1.)).unwrap();
     assert!(q == arr0(22));
 }
 
 #[test]
 fn test_quantile_axis_skipnan_mut_higher_opt_i32() {
     let mut a = arr2(&[[Some(4), Some(2), None, Some(1), Some(5)], [None; 5]]);
-    let q = a.quantile_axis_skipnan_mut::<Higher>(Axis(1), n64(0.6));
+    let q = a.quantile_axis_skipnan_mut::<Higher>(Axis(1), n64(0.6)).unwrap();
     assert_eq!(q.shape(), &[2]);
     assert_eq!(q[0], Some(4));
     assert!(q[1].is_none());
@@ -166,7 +165,7 @@ fn test_quantile_axis_skipnan_mut_higher_opt_i32() {
 #[test]
 fn test_quantile_axis_skipnan_mut_nearest_opt_i32() {
     let mut a = arr2(&[[Some(4), Some(2), None, Some(1), Some(5)], [None; 5]]);
-    let q = a.quantile_axis_skipnan_mut::<Nearest>(Axis(1), n64(0.6));
+    let q = a.quantile_axis_skipnan_mut::<Nearest>(Axis(1), n64(0.6)).unwrap();
     assert_eq!(q.shape(), &[2]);
     assert_eq!(q[0], Some(4));
     assert!(q[1].is_none());
@@ -175,7 +174,7 @@ fn test_quantile_axis_skipnan_mut_nearest_opt_i32() {
 #[test]
 fn test_quantile_axis_skipnan_mut_midpoint_opt_i32() {
     let mut a = arr2(&[[Some(4), Some(2), None, Some(1), Some(5)], [None; 5]]);
-    let q = a.quantile_axis_skipnan_mut::<Midpoint>(Axis(1), n64(0.6));
+    let q = a.quantile_axis_skipnan_mut::<Midpoint>(Axis(1), n64(0.6)).unwrap();
     assert_eq!(q.shape(), &[2]);
     assert_eq!(q[0], Some(3));
     assert!(q[1].is_none());
@@ -184,7 +183,7 @@ fn test_quantile_axis_skipnan_mut_midpoint_opt_i32() {
 #[test]
 fn test_quantile_axis_skipnan_mut_linear_f64() {
     let mut a = arr2(&[[1., 2., ::std::f64::NAN, 3.], [::std::f64::NAN; 4]]);
-    let q = a.quantile_axis_skipnan_mut::<Linear>(Axis(1), n64(0.75));
+    let q = a.quantile_axis_skipnan_mut::<Linear>(Axis(1), n64(0.75)).unwrap();
     assert_eq!(q.shape(), &[2]);
     assert!((q[0] - 2.5).abs() < 1e-12);
     assert!(q[1].is_nan());
@@ -193,7 +192,7 @@ fn test_quantile_axis_skipnan_mut_linear_f64() {
 #[test]
 fn test_quantile_axis_skipnan_mut_linear_opt_i32() {
     let mut a = arr2(&[[Some(2), Some(4), None, Some(1)], [None; 4]]);
-    let q = a.quantile_axis_skipnan_mut::<Linear>(Axis(1), n64(0.75));
+    let q = a.quantile_axis_skipnan_mut::<Linear>(Axis(1), n64(0.75)).unwrap();
     assert_eq!(q.shape(), &[2]);
     assert_eq!(q[0], Some(3));
     assert!(q[1].is_none());
@@ -297,11 +296,12 @@ fn check_one_interpolation_method_for_quantiles_axis_mut<I: Interpolate<u64>>(mu
     let bulk_quantiles = v.quantiles_axis_mut::<I>(axis, &quantile_indexes);
 
     if v.len() == 0 {
-        true
+        bulk_quantiles.is_none()
     } else {
+        let bulk_quantiles = bulk_quantiles.unwrap();
         let mut checks = vec![];
         for quantile_index in quantile_indexes.iter() {
-            let quantile = v.quantile_axis_mut::<I>(axis, *quantile_index);
+            let quantile = v.quantile_axis_mut::<I>(axis, *quantile_index).unwrap();
             checks.push(
                 quantile == *bulk_quantiles.get(quantile_index).unwrap()
             );
