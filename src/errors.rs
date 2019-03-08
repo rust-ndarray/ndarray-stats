e65ef6173f6dd508fcfa1c42c64e2ceef58c8faf
LukeMathWalker <lpalmieri@truelayer.com>
2019-03-08 17:29:30 +0000

    Add docs to error

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,8 @@
 #[derive(Fail, Debug)]
 #[fail(display = "Array shapes do not match: {:?} and {:?}.", first_shape, second_shape)]
+/// An error used by methods and functions that take two arrays as argument and
+/// expect them to have exactly the same shape
+/// (e.g. `ShapeMismatch` is raised when `a.shape() == b.shape()` evaluates to `False`).
 pub struct ShapeMismatch {
     pub first_shape: Vec<usize>,
     pub second_shape: Vec<usize>,
