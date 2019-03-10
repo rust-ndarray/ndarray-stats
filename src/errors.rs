e9679fa46e19780ea41c9edd4b1499ea21f72ad7
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-03-10 10:39:05 +0000

    Use Error instead of Fail

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,7 @@
-#[derive(Fail, Debug)]
-#[fail(display = "Array shapes do not match: {:?} and {:?}.", first_shape, second_shape)]
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
 /// An error used by methods and functions that take two arrays as argument and
 /// expect them to have exactly the same shape
 /// (e.g. `ShapeMismatch` is raised when `a.shape() == b.shape()` evaluates to `False`).
@@ -7,3 +9,11 @@ pub struct ShapeMismatch {
     pub first_shape: Vec<usize>,
     pub second_shape: Vec<usize>,
 }
+
+impl fmt::Display for ShapeMismatch {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Array shapes do not match: {:?} and {:?}.", self.first_shape, self.second_shape)
+    }
+}
+
+impl Error for ShapeMismatch {}
