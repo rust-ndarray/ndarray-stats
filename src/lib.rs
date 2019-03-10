b8ed3ede99cab4d0fa05d037f6f17feea9bc68e2
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-03-10 10:44:20 +0000

    Fix TOC

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,13 @@
 //! the *n*-dimensional array data structure provided by [`ndarray`].
 //!
 //! Currently available routines include:
-//! - [`order statistics`] (minimum, maximum, quantiles, etc.);
-//! - [`partitioning`];
-//! - [`correlation analysis`] (covariance, pearson correlation);
-//! - [`histogram computation`].
+//! - [order statistics] (minimum, maximum, quantiles, etc.);
+//! - [partitioning];
+//! - [correlation analysis] (covariance, pearson correlation);
+//! - [measures from information theory] (entropy, KL divergence, etc.);
+//! - [histogram computation].
 //!
-//! Please feel free to contribute new functionality! A roadmap can be found [`here`].
+//! Please feel free to contribute new functionality! A roadmap can be found [here].
 //!
 //! Our work is inspired by other existing statistical packages such as
 //! [`NumPy`] (Python) and [`StatsBase.jl`] (Julia) - any contribution bringing us closer to
@@ -15,11 +16,12 @@
 //!
 //! [`ndarray-stats`]: https://github.com/jturner314/ndarray-stats/
 //! [`ndarray`]: https://github.com/rust-ndarray/ndarray
-//! [`order statistics`]: trait.QuantileExt.html
-//! [`partitioning`]: trait.Sort1dExt.html
-//! [`correlation analysis`]: trait.CorrelationExt.html
-//! [`histogram computation`]: histogram/index.html
-//! [`here`]: https://github.com/jturner314/ndarray-stats/issues/1
+//! [order statistics]: trait.QuantileExt.html
+//! [partitioning]: trait.Sort1dExt.html
+//! [correlation analysis]: trait.CorrelationExt.html
+//! [measures from information theory]: trait.EntropyExt.html
+//! [histogram computation]: histogram/index.html
+//! [here]: https://github.com/jturner314/ndarray-stats/issues/1
 //! [`NumPy`]: https://docs.scipy.org/doc/numpy-1.14.1/reference/routines.statistics.html
 //! [`StatsBase.jl`]: https://juliastats.github.io/StatsBase.jl/latest/
 
