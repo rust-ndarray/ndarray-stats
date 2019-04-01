2c9030985ffc2f287281e35005e9cef4fe465f6c
Jim Turner <github@turner.link>
2019-04-01 00:39:01 -0400

    Add get_from_sorted_mut benchmark

--- benches/sort.rs
+++ benches/sort.rs
@@ -11,6 +11,31 @@ use ndarray::prelude::*;
 use ndarray_stats::Sort1dExt;
 use rand::prelude::*;
 
+fn get_from_sorted_mut(c: &mut Criterion) {
+    let lens = vec![10, 100, 1000, 10000];
+    let benchmark = ParameterizedBenchmark::new(
+        "get_from_sorted_mut",
+        |bencher, &len| {
+            let mut rng = StdRng::seed_from_u64(42);
+            let mut data: Vec<_> = (0..len).collect();
+            data.shuffle(&mut rng);
+            let indices: Vec<_> = (0..len).step_by(len / 10).collect();
+            bencher.iter_batched(
+                || Array1::from(data.clone()),
+                |mut arr| {
+                    for &i in &indices {
+                        black_box(arr.get_from_sorted_mut(i));
+                    }
+                },
+                BatchSize::SmallInput,
+            )
+        },
+        lens,
+    )
+    .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    c.bench("get_from_sorted_mut", benchmark);
+}
+
 fn get_many_from_sorted_mut(c: &mut Criterion) {
     let lens = vec![10, 100, 1000, 10000];
     let benchmark = ParameterizedBenchmark::new(
@@ -37,6 +62,6 @@ fn get_many_from_sorted_mut(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = get_many_from_sorted_mut
+    targets = get_from_sorted_mut, get_many_from_sorted_mut
 }
 criterion_main!(benches);
