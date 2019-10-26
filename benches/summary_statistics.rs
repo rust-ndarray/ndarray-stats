d9483eaa1133ef4bcf57a99bb15d1df29d223075
Nil Goyette <nil.goyette@imeka.ca>
2019-10-26 05:46:35 -0400

    Weighted var (#55)
    * Move summary statistic tests outside
    
    * Add weighted variance and standard deviation
    
    * Add tests
    
    * Add axis versions
    
    * Add tests for axis versions
    
    * ddof expect and doc
    
    * Fmt
    
    * Add benches

---
+++ benches/summary_statistics.rs
@@ -0,0 +1,37 @@
+use criterion::{
+    black_box, criterion_group, criterion_main, AxisScale, BatchSize, Criterion,
+    ParameterizedBenchmark, PlotConfiguration,
+};
+use ndarray::prelude::*;
+use ndarray_rand::RandomExt;
+use ndarray_stats::SummaryStatisticsExt;
+use rand::distributions::Uniform;
+
+fn weighted_std(c: &mut Criterion) {
+    let lens = vec![10, 100, 1000, 10000];
+    let benchmark = ParameterizedBenchmark::new(
+        "weighted_std",
+        |bencher, &len| {
+            let data = Array::random(len, Uniform::new(0.0, 1.0));
+            let mut weights = Array::random(len, Uniform::new(0.0, 1.0));
+            weights /= weights.sum();
+            bencher.iter_batched(
+                || data.clone(),
+                |arr| {
+                    black_box(arr.weighted_std(&weights, 0.0).unwrap());
+                },
+                BatchSize::SmallInput,
+            )
+        },
+        lens,
+    )
+    .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    c.bench("weighted_std", benchmark);
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = weighted_std
+}
+criterion_main!(benches);
