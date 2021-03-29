56b1e1be24546344a8c94dba3d75337d10fa05ba
Virgile Andreani <armavica@ulminfo.fr>
2021-03-29 02:53:46 +0200

    Remove deprecation in criterion
    ParameterizedBenchmark was deprecated and replaced with BenchmarkGroup.

--- benches/deviation.rs
+++ benches/deviation.rs
@@ -1,5 +1,5 @@
 use criterion::{
-    black_box, criterion_group, criterion_main, AxisScale, Criterion, ParameterizedBenchmark,
+    black_box, criterion_group, criterion_main, AxisScale, Criterion,
     PlotConfiguration,
 };
 use ndarray::prelude::*;
@@ -9,18 +9,21 @@ use ndarray_rand::rand_distr::Uniform;
 
 fn sq_l2_dist(c: &mut Criterion) {
     let lens = vec![10, 100, 1000, 10000];
-    let benchmark = ParameterizedBenchmark::new(
-        "sq_l2_dist",
-        |bencher, &len| {
-            let data = Array::random(len, Uniform::new(0.0, 1.0));
-            let data2 = Array::random(len, Uniform::new(0.0, 1.0));
+    let mut group = c.benchmark_group("sq_l2_dist");
+    group.plot_config(
+        PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    for len in &lens {
+        group.bench_with_input(
+            format!("{}", len),
+            len,
+            |b, &len| {
+                let data = Array::random(len, Uniform::new(0.0, 1.0));
+                let data2 = Array::random(len, Uniform::new(0.0, 1.0));
 
-            bencher.iter(|| black_box(data.sq_l2_dist(&data2).unwrap()))
-        },
-        lens,
-    )
-    .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-    c.bench("sq_l2_dist", benchmark);
+                b.iter(|| black_box(data.sq_l2_dist(&data2).unwrap()))
+            });
+    }
+    group.finish();
 }
 
 criterion_group! {
